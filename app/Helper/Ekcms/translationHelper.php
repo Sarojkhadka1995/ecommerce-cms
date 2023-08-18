83606170a2177d75e7562d5ff65ee88b66372c01
Nirjar Maharjan <nirjar.maharjan@ekbana.info>
2023-08-18 10:43:28 +0545

    language and translation file system and upload excel

--- app/Helper/Ekcms/translationHelper.php
+++ app/Helper/Ekcms/translationHelper.php
@@ -13,44 +13,48 @@ function translate($content, $data = [])
 
     //$translations = array_keys(Locale::getTranslations(Cookie::get('lang') ?? 'en'));
 
-    $lang = app()->getLocale();
+    $langShortCodes = Language::pluck('language_code')->toArray();
+
 
     $directory = resource_path('lang');
     if (!is_dir($directory)) {
         \File::makeDirectory($directory, $mode = 0755, true);
     }
+    foreach ($langShortCodes as $lang) {
 
-    $jsonFileName = "{$lang}.json";
-    $jsonFilePath = "{$directory}/{$jsonFileName}";
+        $jsonFileName = "{$lang}.json";
+        $jsonFilePath = "{$directory}/{$jsonFileName}";
 
-    if (file_exists($jsonFilePath)) {
-        $existingContent = file_get_contents($jsonFilePath);
-        $existingTranslations = json_decode($existingContent, true);
-        if (json_last_error() === JSON_ERROR_NONE) {
-            // Check if the translation key already exists
-            if (!isset($existingTranslations[$key])) {
-                $newTranslations = [
-                    $key => $content,
-                ];
+        if (file_exists($jsonFilePath)) {
+            $existingContent = file_get_contents($jsonFilePath);
+            $existingTranslations = json_decode($existingContent, true);
+            if (json_last_error() === JSON_ERROR_NONE) {
+                // Check if the translation key already exists
+                if (!isset($existingTranslations[$key])) {
+                    $newTranslations = [
+                        $key => $content,
+                    ];
 
-                $mergedTranslations = array_merge($existingTranslations, $newTranslations);
+                    $mergedTranslations = array_merge($existingTranslations, $newTranslations);
 
-                $jsonContentString = json_encode($mergedTranslations, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-                file_put_contents($jsonFilePath, $jsonContentString);
+                    $jsonContentString = json_encode($mergedTranslations, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+                    file_put_contents($jsonFilePath, $jsonContentString);
+                }
             }
         } else {
-            // Handle JSON decoding error if necessary
-        }
-    } else {
-        // Create a new JSON file with initial translations
-        $initialTranslations = [
-            $key => $content,
-        ];
+            // Create a new JSON file with initial translations
+            $initialTranslations = [
+                $key => $content,
+            ];
 
-        $jsonContentString = json_encode($initialTranslations, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        file_put_contents($jsonFilePath, $jsonContentString);
+            $jsonContentString = json_encode($initialTranslations, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            file_put_contents($jsonFilePath, $jsonContentString);
+        }
     }
 
+    $locale = app()->getLocale();
+    $jsonFileName = "{$locale}.json";
+    $jsonFilePath = "{$directory}/{$jsonFileName}";
     $translationsJSON = file_get_contents($jsonFilePath);
 
 // Convert JSON to an associative array
--- app/Http/Controllers/System/language/TranslationController.php
+++ app/Http/Controllers/System/language/TranslationController.php
@@ -66,6 +66,7 @@ class TranslationController extends ResourceController
         if (!in_array($fileExtension, ['xlsx', 'xls'])) {
             return back()->withErrors(['alert-danger' => 'The file type must be xls or xlsx!']);
         }
+
         try {
             $contents = \Excel::import(new TranslationImport(), $file);
             $uploadedData = $contents->toArray($contents, $file);
@@ -74,14 +75,15 @@ class TranslationController extends ResourceController
                 return back()->withErrors(['alert-danger' => 'The file does not contain any translation content']);
             }
             $heading = $this->removeSpacesHeading($uploadedData[0][0]);
-            $langShortCodes = Language::where('group', 'backend')->pluck('language_code')->toArray();
+            $langShortCodes = Language::pluck('language_code')->toArray();
+
             array_unshift($langShortCodes, 'key');
 
             $checkValid = array_diff($heading, $langShortCodes);
+
             if (count($checkValid) > 0) {
                 return back()->withErrors(['alert-danger' => 'Invalid translation content or the provided language may not be available.']);
             }
-
             unset($uploadedData[0][0]); // removing header content from file
 
             $this->parseAndUploadData($uploadedData, $heading);
@@ -104,28 +106,40 @@ class TranslationController extends ResourceController
 
     public function parseAndUploadData($data, $heading)
     {
-        $arrayT = [];
+        $langShortCodes = Language::pluck('language_code')->toArray();
+        $directory = resource_path('lang');
+        if (!is_dir($directory)) {
+            \File::makeDirectory($directory, $mode = 0755, true);
+        }
+        foreach ($langShortCodes as $lang) {
+            $jsonFileName = "{$lang}.json";
+            $jsonFilePath = "{$directory}/{$jsonFileName}";
 
-        foreach ($data[0] as $key => $value) {
-            $word = strtolower(trim(str_replace('.', '', $value[0])));
-            $lang = Locale::where('key', $word)->first();
-            $updated = $this->formatText($value, $heading);
-            if (isset($lang) || $lang !== null) {
-                $lang->update([
-                    'text' => $updated,
-                ]);
-            } else {
-                Locale::create([
-                    'key' => $word,
-                    'text' => $updated,
-                ]);
+            if (file_exists($jsonFilePath) && in_array($lang, $heading)) {
+                $existingContent = file_get_contents($jsonFilePath);
+                $existingTranslations = json_decode($existingContent, true);
+
+                $filteredTrans = $this->getTranslation($heading, $lang, $data[0]);
+
+                $filteredData = [];
+                foreach ($filteredTrans as $key => $translation) {
+                    if (isset($existingTranslations[$key])) {
+                        $filteredData[$key] = $translation;
+
+                        $mergedTranslations = array_merge($existingTranslations, $filteredData);
+
+                        $jsonContentString = json_encode($mergedTranslations, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+                        file_put_contents($jsonFilePath, $jsonContentString);
+                    }
+                }
             }
         }
 
-        return $arrayT;
+        return true;
     }
 
-    public function formatText($data, $heading)
+    public
+    function formatText($data, $heading)
     {
         unset($data[0]); // removing key field
         $arrayT = [];
@@ -134,5 +148,28 @@ class TranslationController extends ResourceController
         }
 
         return $arrayT;
+    }
+
+// Define a function to filter translations by language code
+    function getTranslation($heading, $languageCode, $translationsArray)
+    {
+        // Find the index of the language in the heading array
+        $langIndex = array_search($languageCode, $heading);
+
+        if ($langIndex !== false) {
+            // Initialize an array to store filtered data
+            $filteredData = [];
+
+            // Iterate through the data arrays
+            foreach ($translationsArray as $dataArray) {
+                // Check if the current data array has a translation for the specified language
+                if (isset($dataArray[$langIndex])) {
+                    // Add the translation to the filtered data array
+                    $filteredData[$dataArray[0]] = $dataArray[$langIndex];
+                }
+            }
+
+            return $filteredData;
+        }
     }
 }
